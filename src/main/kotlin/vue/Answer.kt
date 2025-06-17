33be575120bebbd520614443a5d0efb953ad9d61
E247922F <E247922F>
2025-06-17 19:26:29 +0200

    centrage boutons vue Answer

--- src/main/kotlin/vue/Answer.kt
+++ src/main/kotlin/vue/Answer.kt
@@ -1,5 +1,6 @@
 package vue
 
+import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.HBox
@@ -27,5 +28,7 @@ class Answer(question : String) : VBox (){
         contain = VBox()
         contain.children.addAll(btnOui, btnNon)
         this.children.addAll(this.question, contain)
+        contain.alignment = Pos.CENTER
+        this.alignment = Pos.CENTER
     }
 }
